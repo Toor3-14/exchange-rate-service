c20a64e29cdcb81a8db35f43d777dc7d553dedf0
Dmitry <toorkeor@gmail.com>
2023-02-28 00:13:11 -0800

    init

--- src/main/java/com/alfabank/exchange/repository/GiphyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alfabank.exchange.repository;
-
-
-import com.alfabank.exchange.repository.model.Giphy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GiphyRepository extends JpaRepository <Giphy, Long> {
-}
